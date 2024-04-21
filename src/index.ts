12e150e353a545db2cba6227efb775279166ca9a
kravetsone <57632712+kravetsone@users.noreply.github.com>
2024-04-21 08:59:11 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { bearer } from "@elysiajs/bearer";
+import { cors } from "@elysiajs/cors";
+import { jwt } from "@elysiajs/jwt";
+import { swagger } from "@elysiajs/swagger";
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+	.use(swagger())
+	.use(bearer())
+	.use(cors())
+	.use(jwt({ secret: process.env.JWT_SECRET as string }));
+
+app.listen(process.env.PORT as string, () =>
+	console.log(`🦊 Server started at ${app.server?.url.origin}`),
+);
